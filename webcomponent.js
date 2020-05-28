ef7b7a442d8c5ea6ae1cc9f5c540586db1ea7b81
Jonah-T <66018841+Jonah-T@users.noreply.github.com>
2020-05-28 09:38:53 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,26 +1,25 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-
     `;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
 
 
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            		this._firstConnection = false;
-			this._tagContainer;
-			this._tagType = "h1";
-			this._tagText = "Hello World";			
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
+            this._tagContainer;
+            this._tagType = "h1";
+            this._tagText = "Hello World";
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this._firstConnection = true;
-            this.redraw();
+            this.redraw();           
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -42,28 +41,31 @@
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
+        
         }
 
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        // Commented out by default
         /*
         onCustomWidgetResize(width, height){
-            redraw()
+        
         }
         */
 
         redraw(){
-	    if (this._tagText != null){
-		if (this._tagContainer){
-		    this._tagContainer.parentNode.removeChild(this._tagContainer);
-		}
-	this._tagContainer = document.createElement(this._tagType);
-        var theText = document.createTextNode(this._tagText);    
-        this._tagContainer.appendChild(theText); 
-        this._shadowRoot.appendChild(this._tagContainer);
-	}
+            if (this._tagContainer){
+                this._tagContainer.parentNode.removeChild(this._tagContainer);
+            }
+
+            var shadow = window.getSelection(this._shadowRoot);
+            this._tagContainer = document.createElement(this._tagType);
+            var theText = document.createTextNode(this._tagText);    
+            this._tagContainer.appendChild(theText); 
+            this._shadowRoot.appendChild(this._tagContainer);
+
+        }
+    
     
     });
         
